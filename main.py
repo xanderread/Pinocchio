5e3c895be96add7c156119574b4d09039f493417
tonydalziel <92087380+tonydalziel@users.noreply.github.com>
2022-07-05 21:20:02 +0100

    Almost fully functional, only needs 2 more methods and the AI to be implmeneted

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import re
 #TODO: implement ai to determine the liar value calculations -> Use outcome of methods to determine whether the person is a liar
 #TODO: tie program to ai
 
+#The AI will use the different charactersitic changes to determine whether the person is a liar
+#The AI will be provided with the following array:
+# [post length, # sentences, personal pronouns, tensehopping, third person pronouns, avg word length, leaving fast]
+
 def logTexts():
     responses = []
     uncomplete = True
@@ -18,43 +22,40 @@ def logTexts():
         if(text == "q"):
             uncomplete = False
         else:
-            #Must look for outliers as some people may just be slow repliers
-            responseTime = int(input("How much time, in seconds, did it take for the person to respond? "))
-            newText = [text,responseTime]
-            responses.append(newText)
+            #Response time isn't currently being used in the model
+            #responseTime = int(input("How much time, in seconds, did it take for the person to respond? "))
+            #newText = [text,responseTime]
+            responses.append(text)
     return np.array(responses)
 
 
 def main(methods):
-    liarValue = int(50) #AI could determine the liar value
-    #Each potential sign will increase or decrease this value before it is evaluated at the end
+    liarValue = 0
     print("Enter the initial conversation (introducing the game and themselves) below:")
     initialTexts = logTexts()
     print("Enter the conversation after the lie has been mentioned below:")
     lieTexts = logTexts()
-#     initialTexts = np.array([["Hello", 10],["I'm doing good thank you",20],["This. is just a normal conversation yea",50]])
-#     lieTexts = np.array([["I disagree with that",40],["No your lying to me. I didnt say that. Why dont you believe me",50],["I dont want to respond anymore. Loser head.",80]])
-#     print("\n\n\n\n -----------------------------")
     endEarly = input("Did the person abruptly leave, or attempt to abruptly leave, the conversation? Y/N")
     if(endEarly == "Y"):
-        liarValue = liarValue * 1.1
+        liarValue = 1
     print("Evaluating.....")
     for f in methods:
-        liarValue = f(initialTexts,lieTexts,liarValue)
-        print("Current liar value: ",liarValue)
-    print("The final liar value is: ",liarValue)
+        liarArray = [0,0,0,0,0,0,0,0,liarValue]
+        f(initialTexts,lieTexts)
+        print("Current liar array: ",liarArray)
+        #The liar array is then put into the trained AI which will be trained to determine whether the person is lieing or telling the truth based upon these characteristics
     
-def responseTime(initialTexts,lieTexts,liarValue):
-    justTimesInitial = np.array(initialTexts[:,1], dtype=np.uint32)
-    justTimesPost = np.array(lieTexts[:,1], dtype=np.uint32)
-    initialTextResponseTime = np.average(justTimesInitial)
-    postTextReponseTime = np.average(justTimesPost)
-    if(1.1*(initialTextResponseTime) <= (postTextReponseTime)): #Greater than 10% as suggested in wikiHow
-        return liarValue * 1.1
-    else:
-        return liarValue * 0.9
+# def responseTime(initialTexts,lieTexts,liarValue): #Not provided in dataset however can be used to skew the results after applying the AI
+#     justTimesInitial = np.array(initialTexts[:,1], dtype=np.uint32)
+#     justTimesPost = np.array(lieTexts[:,1], dtype=np.uint32)
+#     initialTextResponseTime = np.average(justTimesInitial)
+#     postTextReponseTime = np.average(justTimesPost)
+#     if(1.1*(initialTextResponseTime) <= (postTextReponseTime)): #Greater than 10% as suggested in wikiHow
+#         return liarValue * 1.1
+#     else:
+#         return liarValue * 0.9
 
-def complicated(initialTexts,lieTexts,liarValue):
+def complicated(initialTexts,lieTexts):
     mylen = np.vectorize(len)
     initialLength = np.mean(mylen(initialTexts[:, 0]))
     postLength = np.mean(mylen(lieTexts[:,0]))
@@ -66,10 +67,8 @@ def complicated(initialTexts,lieTexts,liarValue):
     for sentence in lieTexts[:,0]:
         sentences.append(len(sentence.split('. ')))
     postSentences = np.mean(np.array(sentences))
-    if(postLength>initialLength or initialSentences > postSentences):
-        return liarValue * 1.1 #TODO: how do we know to use these excat values? can use AI to get on this
-    else:
-        return liarValue * 0.9 #TODO: how do we know to use these exact values? can use AI to get on this ECT
+    liarArray[0] = postLength / initialLength
+    liarArray[1] = postSentences / initialSentences
 
 def personalPronouns(initialTexts,lieTexts,liarValue):
     #Must assess whether the liar has removed personal pronouns
@@ -82,31 +81,44 @@ def personalPronouns(initialTexts,lieTexts,liarValue):
     for sentence in lieTexts[:,0]:
         pronouns.append(len(pronounRegex.findall(sentence)))
     liePronouns = np.mean(np.array(pronouns))
-    if(initialPronouns > 1.2*liePronouns): #TODO: Must find how much it should change by to be flagged
-        return liarValue * 1.1 #Could instead return how much its increased / decreased by and use that to determine whether they are a liar
-    else:
-        return liarValue * 0.9
+    lieArray[2]=liePronouns / initialPronouns
 
 def tenseHopping(initialTexts,lieTexts,liarValue):
-    #Must assess whether the liar has started tense hopping
+    #Must assess whether the liar has started tense hopping (Google docs contains a Stack overflow post which should help this be solved easily -> Check if tenses are changed between sentences more or less regularly
     return liarValue
 
-def thirdPersonPronouns(initialTexts,lieTexts,liarValue):
+def thirdPersonPronouns(initialTexts,lieTexts):
     #Must assess whether the liar has used more third person pronouns
-    return liarValue
+    pronounRegex = re.compile(r'\b(he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves)\b',re.I)
+    pronouns = []
+    for sentence in initialTexts[:,0]:
+        pronouns.append(len(pronounRegex.findall(sentence)))
+    initialPronouns = np.mean(np.array(pronouns)) #TODO: Should use np implementation throughout with additional time
+    pronouns = []
+    for sentence in lieTexts[:,0]:
+        pronouns.append(len(pronounRegex.findall(sentence)))
+    liePronouns = np.mean(np.array(pronouns))
+    lieArray[4]= liePronouns / initialPronouns
+    
+def avgWordLength(intialTexts,lieTexts):
+    return None
 
-def exclusiveWords(initialTexts,lieTexts,liarValue):
-    #Must assess whether the liar has used fewer exclusive words
-    return liarValue
+# def exclusiveWords(initialTexts,lieTexts,liarValue):
+#     #Must assess whether the liar has used fewer exclusive words
+#     return liarValue
 
-def negativeEmotion(initialTexts,lieTexts,liarValue):
-    #Must assess whether the liar has used more negative emotion words
-    return liarValue
+# def negativeEmotion(initialTexts,lieTexts,liarValue):
+#     #Must assess whether the liar has used more negative emotion words
+#     return liarValue
 
-def motionVerbs(initialTexts,lieTexts,liarValue):
-    #Must assess whether the liar has used more motionVerbs
-    return liarValue
+# def motionVerbs(initialTexts,lieTexts,liarValue):
+#     #Must assess whether the liar has used more motionVerbs
+#     return liarValue
 
-methods = [complicated,responseTime] #Order is in reverse order of importance
+def AIdetection():
+    #Before developing this method, the data must be scraped from the document found and 200+ cases should be made to determine whether someone is a liar or not a liar based on past texts and future texts
+    return None
+
+methods = [complicated,personalPronouns,thirdPersonPronouns,avgWordLength,tenseHopping] #Order is irrelevant
     
 main(methods)
